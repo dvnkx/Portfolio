d0dd89d18327dbba99338f76c504fd1e6610f908
Bogdan <dvnkxxxx@gmail.com>
2023-07-16 19:57:12 +0300

    added theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { NavBar } from "./Components/index";
 
 function App() {
   return (
-    <div className="w-full bg-secondary-100">
+    <div className="w-full bg-secondary-400 dark:bg-secondary-100">
       <NavBar />
     </div>
   );
