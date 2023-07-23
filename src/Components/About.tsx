9869bac946d10cd8468ad9d6d5665497866dda4f
Bogdan <dvnkxxxx@gmail.com>
2023-07-24 00:03:00 +0300

    responsive about

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -9,8 +9,8 @@ const About = () => {
         </h4>
         <hr className="ml-4 w-full h-px border-0 bg-gradient-to-l from-indigo-500" />
       </div>
-      <section className="flex mt-12 ml-11">
-        <p className="text-words-100 text-lg font-light tracking-wide w-4/6 mr-15">
+      <section className="flex mt-12 ml-11 max-md:flex-wrap max-md:ml-0">
+        <p className="text-words-100 text-lg font-light tracking-wide w-4/6 mr-15 max-md:w-full max-md:m-0 ">
           Get a brief look at who I am and what I do. If you would like to know
           more about me and my interests, you can.
         </p>
@@ -18,9 +18,9 @@ const About = () => {
           target="_blank"
           rel="noopener noreferrer"
           href="https://github.com/dvnkx"
-          className="flex items-center"
+          className="flex items-center max-md:mt-10"
         >
-          <h1 className="text-words-100 tracking-wide text-4xl">
+          <h1 className="dark:text-words-100 text-primary tracking-wide text-4xl font-light">
             More about me
           </h1>
           <svg
@@ -38,9 +38,10 @@ const About = () => {
             />
           </svg>
         </a>
+        <img className="hidden max-md:block max-md:mt-10 mr-auto" src={mads} />
       </section>
-      <section className="flex mt-12 ml-11 justify-between">
-        <div className="flex-none w-4/6">
+      <section className="flex mt-12 ml-11 justify-between max-md:ml-0">
+        <div className="flex-none w-4/6 max-md:w-full">
           <h4 className="text-primary text-1xl font-light tracking-wide">
             WHO AM I
           </h4>
@@ -63,18 +64,18 @@ const About = () => {
             I use for many hours daily. It allows me to have it exactly as I
             desire.
           </p>
+          <img className="w-[300px] h-[300px] max-md:hidden" src={mads} />
         </div>
-        <img src={mads} />
       </section>
-      <section className="mt-12 ml-11 ">
+      <section className="mt-12 ml-11 max-md:ml-0 ">
         <h4 className="text-primary text-1xl font-light tracking-wide">
           TECH I ENJOY
         </h4>
-        <div className="flex mt-10 gap-4">
+        <div className="flex mt-10 gap-4 max-md:flex-wrap">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 128 128"
-            className="w-9 h-9"
+            className="w-9 h-9 max-md:w-[50px] hover:stroke-secondary-500"
           >
             <path fill="#8f9297" d="M1.408 1.408h125.184v125.185H1.408z" />
             <path
@@ -85,7 +86,7 @@ const About = () => {
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 128 128"
-            className="w-9 h-9"
+            className="w-9 h-9 max-md:w-[50px] hover:stroke-secondary-500"
           >
             <path fill="#323330" d="M22.67 47h99.67v73.67H22.67z" />
             <path
@@ -97,7 +98,7 @@ const About = () => {
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 128 128"
-            className="w-9 h-9"
+            className="w-9 h-9 max-md:w-[50px] hover:stroke-secondary-500"
           >
             <path
               fill="#8f9297"
@@ -127,7 +128,7 @@ const About = () => {
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 128 128"
-            className="w-9 h-9"
+            className="w-9 h-9 max-md:w-[50px] hover:stroke-secondary-500"
           >
             <path
               fill="#8f9297"
@@ -146,7 +147,7 @@ const About = () => {
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 128 128"
-            className="w-9 h-9"
+            className="w-9 h-9 max-md:w-[50px] hover:stroke-secondary-500"
           >
             <g fill="#8f9297">
               <circle cx="64" cy="64" r="11.4" />
@@ -156,7 +157,7 @@ const About = () => {
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 128 128"
-            className="w-9 h-9"
+            className="w-9 h-9 max-md:w-[50px] hover:stroke-secondary-500"
           >
             <path
               fill="#8f9297"
@@ -166,7 +167,7 @@ const About = () => {
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 128 128"
-            className="w-9 h-9"
+            className="w-9 h-9 max-md:w-[50px] hover:stroke-secondary-500"
           >
             <path
               d="M64.004 25.602c-17.067 0-27.73 8.53-32 25.597 6.398-8.531 13.867-11.73 22.398-9.597 4.871 1.214 8.352 4.746 12.207 8.66C72.883 56.629 80.145 64 96.004 64c17.066 0 27.73-8.531 32-25.602-6.399 8.536-13.867 11.735-22.399 9.602-4.87-1.215-8.347-4.746-12.207-8.66-6.27-6.367-13.53-13.738-29.394-13.738zM32.004 64c-17.066 0-27.73 8.531-32 25.602C6.402 81.066 13.87 77.867 22.402 80c4.871 1.215 8.352 4.746 12.207 8.66 6.274 6.367 13.536 13.738 29.395 13.738 17.066 0 27.73-8.53 32-25.597-6.399 8.531-13.867 11.73-22.399 9.597-4.87-1.214-8.347-4.746-12.207-8.66C55.128 71.371 47.868 64 32.004 64zm0 0"
@@ -176,7 +177,7 @@ const About = () => {
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 128 128"
-            className="w-9 h-9"
+            className="w-9 h-9 max-md:w-[50px] hover:stroke-secondary-500"
           >
             <path
               fill="#8f9297"
@@ -186,7 +187,7 @@ const About = () => {
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 128 128"
-            className="w-9 h-9"
+            className="w-9 h-9 max-md:w-[50px] hover:stroke-secondary-500"
           >
             <path
               fillRule="evenodd"
@@ -288,7 +289,7 @@ const About = () => {
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 128 128"
-            className="w-9 h-9"
+            className="w-9 h-9 max-md:w-[50px] hover:stroke-secondary-500"
             fill="#8f9297"
           >
             <path d="M126.67 98.44c-4.56 1.16-7.38.05-9.91-3.75-5.68-8.51-11.95-16.63-18-24.9-.78-1.07-1.59-2.12-2.6-3.45C89 76 81.85 85.2 75.14 94.77c-2.4 3.42-4.92 4.91-9.4 3.7l26.92-36.13L67.6 29.71c4.31-.84 7.29-.41 9.93 3.45 5.83 8.52 12.26 16.63 18.67 25.21 6.45-8.55 12.8-16.67 18.8-25.11 2.41-3.42 5-4.72 9.33-3.46-3.28 4.35-6.49 8.63-9.72 12.88-4.36 5.73-8.64 11.53-13.16 17.14-1.61 2-1.35 3.3.09 5.19C109.9 76 118.16 87.1 126.67 98.44zM1.33 61.74c.72-3.61 1.2-7.29 2.2-10.83 6-21.43 30.6-30.34 47.5-17.06C60.93 41.64 63.39 52.62 62.9 65H7.1c-.84 22.21 15.15 35.62 35.53 28.78 7.15-2.4 11.36-8 13.47-15 1.07-3.51 2.84-4.06 6.14-3.06-1.69 8.76-5.52 16.08-13.52 20.66-12 6.86-29.13 4.64-38.14-4.89C5.26 85.89 3 78.92 2 71.39c-.15-1.2-.46-2.38-.7-3.57q.03-3.04.03-6.08zm5.87-1.49h50.43c-.33-16.06-10.33-27.47-24-27.57-15-.12-25.78 11.02-26.43 27.57z" />
@@ -297,7 +298,7 @@ const About = () => {
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 128 128"
-            className="w-9 h-9"
+            className="w-9 h-9 max-md:w-[50px] hover:stroke-secondary-500"
           >
             <path
               fill="#8f9297"
@@ -307,7 +308,7 @@ const About = () => {
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 128 128"
-            className="w-9 h-9"
+            className="w-9 h-9 max-md:w-[50px] hover:stroke-secondary-500"
           >
             <path
               fill="#8f9297"
@@ -317,7 +318,7 @@ const About = () => {
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 128 128"
-            className="w-9 h-9"
+            className="w-9 h-9 max-md:w-[50px] hover:stroke-secondary-500"
           >
             <path
               fillRule="evenodd"
