44cf5256d68e363af0d9ba88b0581f8dbbf3a0fc
Bogdan <dvnkxxxx@gmail.com>
2023-07-30 00:17:47 +0300

    added about animations

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -1,20 +1,47 @@
 import mads from "../assets/mads.jpg";
+import { motion } from "framer-motion";
+import {
+  projectsAnimation,
+  aboutAnimations,
+  fadeOutFromX,
+  fadeOut,
+} from "../utils/animations";
 
 const About = () => {
   return (
     <section id="about" className="mt-20">
       <div className="flex items-center">
-        <h4 className="text-primary text-1xl font-light tracking-wide">
+        <motion.h4
+          variants={projectsAnimation.titleHeader}
+          initial="initial"
+          animate="animate"
+          className="text-primary text-1xl font-light tracking-wide"
+        >
           ABOUT
-        </h4>
-        <hr className="ml-4 w-full h-px border-0 bg-gradient-to-l from-indigo-500" />
+        </motion.h4>
+        <motion.hr
+          variants={projectsAnimation.sectionHeader}
+          initial="initial"
+          animate="animate"
+          className="ml-4 w-full h-px border-0 bg-gradient-to-l from-indigo-500"
+        />
       </div>
       <section className="flex mt-12 ml-11 max-lg:flex-wrap max-lg:gap-10 max-md:ml-0">
-        <p className="text-words-100 text-lg font-light tracking-wide w-4/6 mr-15 max-md:w-full max-md:m-0 ">
+        <motion.p
+          variants={aboutAnimations.sectionText}
+          custom={0}
+          initial="initial"
+          animate="animate"
+          className="text-words-100 text-lg font-light tracking-wide w-4/6 mr-15 max-md:w-full max-md:m-0 "
+        >
           Get a brief look at who I am and what I do. If you would like to know
           more about me and my interests, you can.
-        </p>
-        <a
+        </motion.p>
+        <motion.a
+          variants={aboutAnimations.sectionText}
+          custom={0}
+          initial="initial"
+          animate="animate"
           target="_blank"
           rel="noopener noreferrer"
           href="https://github.com/dvnkx"
@@ -36,40 +63,95 @@ const About = () => {
               d="M4.5 4.5l15 15m0 0V8.25m0 11.25H8.25"
             />
           </svg>
-        </a>
-        <img className="hidden max-lg:block max-md:mt-10 mr-auto" src={mads} />
+        </motion.a>
+        <motion.img
+          variants={fadeOut}
+          initial="initial"
+          animate="animate"
+          className="hidden max-lg:block max-md:mt-10 mr-auto"
+          src={mads}
+        />
       </section>
-      <section className="flex mt-12 ml-11 justify-between max-md:ml-0">
+      <section className="relative flex mt-12 ml-11 justify-between max-md:ml-0">
         <div className="w-3/6 max-lg:w-full">
-          <h4 className="text-primary text-1xl font-light tracking-wide">
+          <motion.h4
+            variants={projectsAnimation.titleHeader}
+            initial="initial"
+            animate="animate"
+            custom={1}
+            className="text-primary text-1xl font-light tracking-wide"
+          >
             WHO AM I
-          </h4>
-          <p className="text-words-100 tracking-wide font-light text-lg mt-10 leading-relax">
+          </motion.h4>
+          <motion.p
+            variants={aboutAnimations.sectionText}
+            custom={0.3}
+            initial="initial"
+            animate="animate"
+            className="text-words-100 tracking-wide font-light text-lg mt-10 leading-relax"
+          >
             My name is Bogdan Lisniak. I am 21 years old, I live in Kyiv,
             Ukraine, and I&apos;m a self-taught Frontend Developer.
-          </p>
-          <p className="text-words-100 tracking-wide font-light text-lg mt-10 leading-relax">
+          </motion.p>
+          <motion.p
+            variants={aboutAnimations.sectionText}
+            custom={0.5}
+            initial="initial"
+            animate="animate"
+            className="text-words-100 tracking-wide font-light text-lg mt-10 leading-relax"
+          >
             I've always been interested in how a virtual environment works under
             the hood. Out of awareness I was also interested in creating a
             beautiful UI with awesome user experiences.
-          </p>
-          <p className="text-words-100 tracking-wide font-light text-lg mt-10 leading-relax">
+          </motion.p>
+          <motion.p
+            variants={aboutAnimations.sectionText}
+            custom={0.7}
+            initial="initial"
+            animate="animate"
+            className="text-words-100 tracking-wide font-light text-lg mt-10 leading-relax"
+          >
             I enjoy the constant change in the technologies used in the area and
             love diving into new frameworks and technologies.
-          </p>
-          <p className="text-words-100 tracking-wide font-light text-lg mt-10 leading-relax">
+          </motion.p>
+          <motion.p
+            variants={aboutAnimations.sectionText}
+            custom={0.9}
+            initial="initial"
+            animate="animate"
+            className="text-words-100 tracking-wide font-light text-lg mt-10 leading-relax"
+          >
             Spending time customizing, improving and tinkering with my work
             environment and tools is something I enjoy a lot, as it is something
             I use for many hours daily. It allows me to have it exactly as I
             desire.
-          </p>
+          </motion.p>
         </div>
-        <img className="w-[300px] h-[300px] max-lg:hidden " src={mads} />
+        <motion.div
+          variants={fadeOutFromX}
+          initial="initial"
+          animate="animate"
+          custom={1}
+          className="w-[300px] h-[300px] absolute bg-white right-0"
+        />
+        <motion.img
+          variants={fadeOut}
+          custom={1.6}
+          initial="initial"
+          animate="animate"
+          className="w-[300px] h-[300px] max-lg:hidden z-10"
+          src={mads}
+        />
       </section>
       <section className="mt-12 ml-11 max-md:ml-0 ">
-        <h4 className="text-primary text-1xl font-light tracking-wide">
+        <motion.h4
+          variants={projectsAnimation.titleHeader}
+          initial="initial"
+          animate="animate"
+          className="text-primary text-1xl font-light tracking-wide"
+        >
           TECH I ENJOY
-        </h4>
+        </motion.h4>
         <div className="flex mt-10 gap-4 max-md:flex-wrap">
           <svg
             xmlns="http://www.w3.org/2000/svg"
--- src/utils/animations.tsx
+++ src/utils/animations.tsx
@@ -1,3 +1,35 @@
+export const fadeOut = {
+  initial: {
+    opacity: 0,
+  },
+  animate: (delay: number) => ({
+    opacity: 1,
+    transition: {
+      delay: delay,
+      duration: 1,
+      ease: [0.9, 0.1, 0.3, 0.96],
+    },
+  }),
+};
+
+export const fadeOutFromX = {
+  initial: {
+    scaleX: 0,
+    originX: 0,
+    opacity: 0,
+  },
+  animate: (delay: number) => ({
+    scaleX: 1,
+    originX: [0, 0, 0, 0, 0, 0, 0.5],
+    opacity: 1,
+    transition: {
+      delay: delay,
+      duration: 0.8,
+      ease: [0.9, 0.1, 0.3, 0.96],
+    },
+  }),
+};
+
 export const navBarAnimation = {
   initial: {
     opacity: 0,
@@ -91,14 +123,15 @@ export const projectsAnimation = {
       x: -200,
       opacity: 0,
     },
-    animate: {
+    animate: (delay: number) => ({
       x: 0,
       opacity: 1,
       transition: {
         easy: [0.6, 0.01, 0.85, 0.95],
         duration: 1,
+        delay: delay,
       },
-    },
+    }),
   },
 
   textAppearance: {
@@ -160,6 +193,24 @@ export const projectsAnimation = {
         duration: 0.5,
         ease: [0.6, 0.5, 0.5, 0.9],
         delay: 3.2 + duration * 0.5,
+      },
+    }),
+  },
+};
+
+export const aboutAnimations = {
+  sectionText: {
+    initial: {
+      y: 40,
+      opacity: 0,
+    },
+    animate: (delay: number) => ({
+      y: 0,
+      opacity: 1,
+      transition: {
+        delay: 1 + delay * 0.3,
+        duration: 0.7,
+        ease: [0.9, 0.1, 0.3, 0.96],
       },
     }),
   },
