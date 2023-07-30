c84622b90a06fe568800413a2649ab185b1c5a2d
Bogdan <dvnkxxxx@gmail.com>
2023-07-30 14:57:00 +0300

    added footer animations

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,33 +1,44 @@
+import { motion } from "framer-motion";
+import { fadeOut } from "../utils/animations";
+
 const Footer = () => {
+  const links = [
+    { href: "https://github.com/dvnkx", title: "GitHub" },
+    {
+      href: "https://www.linkedin.com/in/bohdan-lisniak-02288526a/",
+      title: " LinkedIn",
+    },
+    { href: "https://djinni.co/q/413f1d5caf/", title: "Djinni" },
+  ];
+
   return (
     <footer className="container flex flex-col justify-center gap-10 py-16 md:flex-row md:justify-between text-words-100">
       <section className="flex w-1/2 gap-8 md:gap-12 text-xl [&>*]:cursor-pointer">
-        <a
-          className="hover:text-primary dark:hover:text-words-200"
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/dvnkx"
-        >
-          GitHub
-        </a>
-        <a
-          className="hover:text-primary dark:hover:text-words-200"
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://www.linkedin.com/in/bohdan-lisniak-02288526a/"
-        >
-          LinkedIn
-        </a>
-        <a
-          className="hover:text-primary dark:hover:text-words-200"
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://djinni.co/q/413f1d5caf/"
-        >
-          Djinni
-        </a>
+        {links.map((link) => {
+          return (
+            <motion.a
+              variants={fadeOut}
+              initial="initial"
+              animate="animate"
+              className="hover:text-primary dark:hover:text-words-200"
+              href={link.href}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {link.title}
+            </motion.a>
+          );
+        })}
       </section>
-      <p className="font-light text-primary"> &#169; Bogdan Lisniak</p>
+      <motion.p
+        variants={fadeOut}
+        initial="initial"
+        animate="animate"
+        className="font-light text-primary"
+      >
+        {" "}
+        &#169; Bogdan Lisniak
+      </motion.p>
     </footer>
   );
 };
