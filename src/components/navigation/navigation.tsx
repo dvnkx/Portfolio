994a252f0c12a45ac83665ea521f6ddf4fc7cee8
Bogdan <dvnkxxxx@gmail.com>
2023-08-16 19:54:17 +0300

    1 try

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -62,7 +62,7 @@ export const Navigation = () => {
       variants={navVariants}
       initial="hidden"
       animate="visible"
-      className="fixed top-0 left-0 bg-transparent z-40 w-full flex h-32 justify-between py-5 px-15 max-md:px-5 max-lg:px-10 max-md:overflow-visible"
+      className="fixed top-0 left-0 bg-transparent z-40 w-full flex h-32 justify-between py-5 px-15 max-md:px-0 max-lg:px-0 max-md:overflow-visible"
     >
       <a className="w-1/7 h-full flex-none" href="/">
         <img
